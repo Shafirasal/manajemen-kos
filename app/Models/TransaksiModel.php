c67be38874a722b9fa19b21de63b4e6dbcb28f6e
Shafirasal <shafirasalsabila15@gmail.com>
2025-11-16 19:38:38 +0700

    model -> tambah model transaksi

--- app/Models/TransaksiModel.php
+++ app/Models/TransaksiModel.php
@@ -2,9 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TransaksiModel extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 't_transaksi';
+    protected $primaryKey = 'id_transaksi';
+
+    public $timestamps = true;
+
+    protected $fillable = [
+        'id_penyewa',
+        'id_kamar',
+        'tanggal_bayar',
+        'tanggal_masuk',
+        'tanggal_keluar',
+        'status'
+    ];
+
+    // transaksi → penyewa
+    public function penyewa()
+    {
+        return $this->belongsTo(PenyewaModel::class, 'id_penyewa');
+    }
+
+    // transaksi → kamar
+    public function kamar()
+    {
+        return $this->belongsTo(KamarModel::class, 'id_kamar');
+    }
 }

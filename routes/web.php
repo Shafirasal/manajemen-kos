f34d9a93fbe704e0d2933385002b443f9dc0e054
Shafirasal <shafirasalsabila15@gmail.com>
2025-11-18 10:37:52 +0700

    eoute -> tambah rute kamar dan transaksi

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\KamarController;
 use App\Http\Controllers\PenyewaController;
 use App\Http\Controllers\TipeKamarController;
+use App\Http\Controllers\TransaksiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -31,4 +33,28 @@ Route::get('/', function () {
         Route::put('/{id}/update', [TipeKamarController::class, 'update']);
         Route::get('/{id}/confirm', [TipeKamarController::class, 'confirm']);
         Route::delete('/{id}/delete', [TipeKamarController::class, 'delete']);
+    });
+
+        Route::prefix('kamar')->name('kamar.')->group(function () {
+        Route::post('/list', [KamarController::class, 'list']);
+        Route::get('/', [KamarController::class, 'index']);
+        Route::get('/create', [KamarController::class, 'create']);
+        Route::post('/store', [KamarController::class, 'store']);
+        Route::get('/{id}/show', [KamarController::class, 'show']);
+        Route::get('/{id}/edit', [KamarController::class, 'edit']);
+        Route::put('/{id}/update', [KamarController::class, 'update']);
+        Route::get('/{id}/confirm', [KamarController::class, 'confirm']);
+        Route::delete('/{id}/delete', [KamarController::class, 'delete']);
+    });
+
+        Route::prefix('transaksi')->name('transaksi.')->group(function () {
+        Route::post('/list', [TransaksiController::class, 'list']);
+        Route::get('/', [TransaksiController::class, 'index']);
+        Route::get('/create', [TransaksiController::class, 'create']);
+        Route::post('/store', [TransaksiController::class, 'store']);
+        Route::get('/{id}/show', [TransaksiController::class, 'show']);
+        Route::get('/{id}/edit', [TransaksiController::class, 'edit']);
+        Route::put('/{id}/update', [TransaksiController::class, 'update']);
+        Route::get('/{id}/confirm', [TransaksiController::class, 'confirm']);
+        Route::delete('/{id}/delete', [TransaksiController::class, 'delete']);
     });

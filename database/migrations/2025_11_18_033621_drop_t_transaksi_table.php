52f76a1d77b828f465846f10f688e8bc111dbfba
Shafirasal <shafirasalsabila15@gmail.com>
2025-11-18 17:41:26 +0700

    migrasi -> drop tabel t_transaksi

--- database/migrations/2025_11_18_033621_drop_t_transaksi_table.php
+++ database/migrations/2025_11_18_033621_drop_t_transaksi_table.php
@@ -1,24 +1,29 @@
+
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
-        //
+        // Drop tabel t_transaksi
+        Schema::dropIfExists('t_transaksi');
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
-        //
+        // Kalau mau restore, bisa bikin ulang minimal struktur dasar
+        Schema::create('t_transaksi', function ($table) {
+            $table->id('id_transaksi');
+            $table->unsignedBigInteger('id_penyewa');
+            $table->unsignedBigInteger('id_kamar');
+            $table->date('tanggal_bayar')->nullable();
+            $table->date('tanggal_masuk');
+            $table->date('tanggal_keluar');
+            $table->enum('status', ['lunas', 'belum_lunas']);
+            $table->timestamps();
+        });
     }
 };

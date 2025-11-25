08bf0a821cdb3a73a0a13b3ae91af2bf86c0070e
Shafirasal <shafirasalsabila15@gmail.com>
2025-11-25 13:49:36 +0700

    model ->  tambah kolom tanggal jatuh tempo

--- app/Models/TransaksiPembayaranModel.php
+++ app/Models/TransaksiPembayaranModel.php
@@ -2,16 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TransaksiPembayaranModel extends Model
 {
-    //
+    use HasFactory;
 
+    protected $table = 't_transaksi_pembayaran';
+    protected $primaryKey = 'id_transaksi_pembayaran';
 
+    protected $fillable = [
+        'id_transaksi_sewa',
+        'tanggal_bayar',
+        'tanggal_jatuh_tempo',
+        'uraian',
+        'nominal',
+        'bukti_bayar',
+        'status'
+    ];
 
-        public function transaksiSewa()
+    public function transaksiSewa()
     {
-        return $this->belongsTo(TransaksiSewaModel::class, 'id_transaksi_sewa', 'id_transaksi_sewa');
+        return $this->belongsTo(TransaksiSewaModel::class, 'id_transaksi_sewa');
     }
 }

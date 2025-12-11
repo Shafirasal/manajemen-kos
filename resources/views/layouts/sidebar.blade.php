776d34ebde5dd723e243b8f9babc3b4e1a6f5b69
Shafirasal <shafirasalsabila15@gmail.com>
2025-12-11 15:27:36 +0700

    sidebar -> ubah rute dashboard

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -11,8 +11,7 @@
       <li class="dropdown">
         <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
         <ul class="dropdown-menu">
-          <li><a class="nav-link" href="index-0.html">General Dashboard</a></li>
-          <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
+            <li><a class="nav-link" href="{{ url('/dashboard') }}">Informasi Umum</a></li>
         </ul>
       </li>
 
